2d4f2012d95862cb3bdbbe0cbaf9f89d2880c744
Fliper Gustavo <gguedes.tech@gmail.com>
2020-04-17 20:43:30 -0300

    web: Main screen

---
+++ frontend/src/pages/Main/index.tsx
@@ -0,0 +1,162 @@
+import React, { useState, useEffect } from "react";
+import { FiSearch, FiPlus, FiX } from "react-icons/fi";
+
+import { ITool, INewTool } from "../../store/types";
+import ToolRepository from "../../store/actions";
+
+import {
+  Container,
+  ToolsList,
+  successBanner,
+  errorBanner,
+  warningBanner,
+} from "./styles";
+import ConfirmToDelete from "../../components/ConfirmToDelete";
+import NewToolForm from "../../components/NewToolForm";
+
+interface Props {
+  toolRepository: ToolRepository;
+}
+
+const Main: React.FC<Props> = ({ toolRepository }) => {
+  const [findInTags, setFindInTags] = useState(false);
+  const [tools, setTools] = useState<ITool[]>([]);
+  const [openNewToolForm, setOpenNewToolForm] = useState(false);
+  const [deleteConfimation, setDeleteConfirmation] = useState(false);
+  const [
+    selectedToolToDelete,
+    setSelectedToolToDelete,
+  ] = useState<ITool | null>();
+  const [searchValue, setSearchValue] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+
+  useEffect(() => {
+    async function loadTools() {
+      setTools(await toolRepository.getTools());
+      successBanner("Welcome to the application");
+    }
+
+    loadTools();
+  }, []);
+
+  async function handleSearchTool() {
+    if (!isSearching) {
+      setIsSearching(true);
+
+      const findedTools = await toolRepository.getToolsByFilter(
+        findInTags,
+        searchValue
+      );
+
+      findedTools.length === 0 && warningBanner("No tools found");
+
+      setTools(findedTools);
+      setIsSearching(false);
+    }
+  }
+
+  async function handleDeleteTool(id: number) {
+    await toolRepository.deleteTool(id);
+
+    const nextState = tools.filter((tool) => tool.id !== id);
+
+    setTools(nextState);
+
+    errorBanner("Tool deleted with success");
+  }
+
+  async function handleAddNewTool(newTool: INewTool) {
+    const addedTool = await toolRepository.addTool(newTool);
+
+    setTools([addedTool, ...tools]);
+
+    successBanner("New tool added with success");
+  }
+
+  return (
+    <Container>
+      <header>
+        <h1>VUTTR</h1>
+        <h4>Very Useful Tools to Remember</h4>
+
+        <div id="searchAndAddTool">
+          <div>
+            <label htmlFor="input">
+              <div id="searchInput">
+                <FiSearch />
+                <input
+                  type="text"
+                  id="input"
+                  placeholder="search"
+                  value={searchValue}
+                  onKeyUp={handleSearchTool}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                />
+              </div>
+            </label>
+
+            <label htmlFor="withTag" className="checkboxArea">
+              <input
+                onChange={() => setFindInTags(!findInTags)}
+                type="checkbox"
+                name="withTag"
+                id="withTag"
+                checked={findInTags}
+              />
+              <span>search in tags only</span>
+            </label>
+          </div>
+          <button onClick={() => setOpenNewToolForm(!openNewToolForm)}>
+            <FiPlus /> <span>Add</span>
+          </button>
+        </div>
+      </header>
+
+      <ToolsList>
+        {tools.map((tool) => (
+          <li key={String(tool.id)}>
+            <header>
+              <h5 className="title">{tool.title}</h5>
+              <button
+                className="deleteButton"
+                onClick={() => {
+                  setSelectedToolToDelete(tool);
+                  setDeleteConfirmation(!deleteConfimation);
+                }}
+              >
+                <FiX />
+                <span>remove</span>
+              </button>
+            </header>
+
+            <p>{tool.description}</p>
+
+            <ul>
+              {tool.tags.map((tool) => (
+                <li key={tool}>#{tool}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ToolsList>
+      {openNewToolForm && (
+        <NewToolForm
+          isOpened={() => setOpenNewToolForm(!openNewToolForm)}
+          addNewTool={handleAddNewTool}
+        />
+      )}
+      {deleteConfimation && selectedToolToDelete && (
+        <ConfirmToDelete
+          tool={selectedToolToDelete}
+          close={() => {
+            setDeleteConfirmation(!deleteConfimation);
+            setSelectedToolToDelete(null);
+          }}
+          requestDelete={handleDeleteTool}
+        />
+      )}
+    </Container>
+  );
+};
+
+export default Main;
---
+++ frontend/src/pages/Main/styles.ts
@@ -0,0 +1,185 @@
+import styled from "styled-components";
+import { css } from "glamor";
+import { toast } from "react-toastify";
+
+import * as Pallete from "../../styles/colors";
+import { BannerNotificationColors } from "../../styles/colors";
+
+export const Container = styled.div`
+  max-width: 800px;
+  margin: 0px auto;
+  padding: 50px 0px;
+
+  header {
+    margin-bottom: 15px;
+
+    h1 {
+      padding-bottom: 25px;
+      color: ${Pallete.white};
+    }
+
+    h4 {
+      padding-bottom: 35px;
+      color: ${Pallete.white};
+    }
+
+    div#searchAndAddTool {
+      display: flex;
+      justify-content: space-between;
+
+      label.checkboxArea {
+        position: relative;
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+
+        span {
+          margin-left: 5px;
+        }
+      }
+
+      div {
+        display: flex;
+        align-items: center;
+
+        > label {
+          margin-right: 5px;
+        }
+      }
+
+      div#searchInput {
+        border: 1px solid ${Pallete.borderColor};
+        border-radius: 5px;
+        background-color: ${Pallete.inputBackgroundColor};
+        padding: 10px 22px;
+        padding-left: 5px;
+        display: flex;
+        align-items: center;
+
+        input {
+          border: 0px;
+          background: transparent;
+          padding-left: 5px;
+        }
+      }
+
+      button {
+        background-color: ${Pallete.buttonPrimaryNeutral.Regular};
+        color: ${Pallete.white};
+        display: flex;
+        align-content: center;
+        transition: all 0.2s linear;
+
+        span {
+          margin-left: 5px;
+          font-weight: bold;
+        }
+
+        &:hover {
+          background-color: ${Pallete.buttonPrimaryNeutral.Hover};
+        }
+
+        &:active {
+          background-color: ${Pallete.buttonPrimaryNeutral.Click};
+        }
+      }
+    }
+  }
+`;
+
+export const ToolsList = styled.ul`
+  > li {
+    box-shadow: 0px 10px 10px #0000000d;
+    background-color: ${Pallete.white};
+    border: 1px solid ${Pallete.borderColor};
+    border-radius: 5px;
+    padding: 15px;
+
+    & + li {
+      margin-top: 15px;
+    }
+
+    > header {
+      display: flex;
+      justify-content: space-between;
+
+      h5 {
+        text-decoration: underline;
+        color: ${Pallete.primaryDarkColor};
+      }
+
+      svg {
+        color: ${Pallete.buttonQuartiaryDanger.Click};
+      }
+
+      button.deleteButton {
+        display: flex;
+        align-items: center;
+        height: 30px;
+        text-align: center;
+        padding-top: 0;
+        padding-right: 0;
+        padding-bottom: 0;
+        padding-left: 0;
+
+        &:hover span {
+          color: ${Pallete.buttonQuartiaryDanger.Hover};
+        }
+
+        &:active span {
+          color: ${Pallete.buttonQuartiaryDanger.Click};
+        }
+
+        span {
+          margin-left: 5px;
+          color: ${Pallete.buttonQuartiaryDanger.Regular};
+        }
+      }
+    }
+
+    > p {
+      max-lines: 2;
+      margin-bottom: 15px;
+    }
+
+    > ul {
+      display: flex;
+      flex-wrap: wrap;
+      max-width: 100%;
+
+      li {
+        font-weight: bold;
+
+        & + li {
+          margin-left: 5px;
+        }
+      }
+    }
+  }
+`;
+
+export function successBanner(msg: string) {
+  return toast.success(msg, {
+    className: css({
+      background: `${BannerNotificationColors.SUCCESS} !important`,
+      borderRadius: "5px !important",
+    }),
+  });
+}
+export function errorBanner(msg: string) {
+  return toast.error(msg, {
+    className: css({
+      background: `${BannerNotificationColors.ERROR} !important`,
+      borderRadius: "5px !important",
+    }),
+  });
+}
+
+export function warningBanner(msg: string) {
+  return toast.error(msg, {
+    className: css({
+      background: `${BannerNotificationColors.WARNING} !important`,
+      borderRadius: "5px !important",
+    }),
+  });
+}
