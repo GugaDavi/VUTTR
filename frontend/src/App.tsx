8abadcc0a120f0870cbd68223d12346115de3f3b
Fliper Gustavo <gguedes.tech@gmail.com>
2020-04-17 20:50:05 -0300

    web: project dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
+
+import Main from "./pages/Main";
+
+import GlobalStyles from "./styles/global";
+import ToolRepository from "./store/actions";
 
 function App() {
-  return <div></div>;
+  const toolRepository = new ToolRepository();
+
+  return (
+    <>
+      <GlobalStyles />
+      <Main toolRepository={toolRepository} />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
